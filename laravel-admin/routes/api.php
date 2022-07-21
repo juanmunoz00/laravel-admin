b285b36ccca6093c7e265d6c447a68fad5c81ba1
Juan Munoz <j.munoz@lacroix.group>
2022-07-21 12:29:53 -0600

    Project created. Ready for migrations. Ready to register user through API.

---
+++ laravel-admin/routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Esta ruta tiene una funcion que regresa un texto "hello"
+
+//Route::get('hello', fn() => 'hello');
+Route::post('register', [AuthController::class, 'register']);
